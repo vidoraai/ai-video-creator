b1ee6f2c3927b21162ef6835e66d4c224bbae345
vidoraai <comgoke@gmail.com>
2026-09-08 07:47:35 +0100

    Create index.js

---
+++ backend/services/index.js
@@ -0,0 +1,33 @@
+const openaiVideoProvider =
+  require("./openaiVideoProvider");
+
+
+const providers = {
+  openai: openaiVideoProvider
+};
+
+
+function getVideoProvider() {
+
+  const providerName =
+    process.env.VIDORA_VIDEO_PROVIDER ||
+    "openai";
+
+  const provider =
+    providers[providerName];
+
+  if (!provider) {
+
+    throw new Error(
+      `Video provider "${providerName}" is not supported.`
+    );
+
+  }
+
+  return provider;
+}
+
+
+module.exports = {
+  getVideoProvider
+};
